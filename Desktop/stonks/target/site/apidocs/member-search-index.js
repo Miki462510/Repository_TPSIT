8237b302ebb885517d1859aa16bc5b136d6c63b0
Miki462510 <78083568+Miki462510@users.noreply.github.com>
2021-02-09 22:39:34 +0100

    first commit

--- Desktop/stonks/target/site/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"URLEncoder","l":"encode(String)","u":"encode(java.lang.String)"},{"p":"<Unnamed>","c":"URLEncoder","l":"encode(String, String)","u":"encode(java.lang.String,java.lang.String)"},{"p":"<Unnamed>","c":"URLEncoder","l":"URLEncoder()","u":"%3Cinit%3E()"}];updateSearchResults();
